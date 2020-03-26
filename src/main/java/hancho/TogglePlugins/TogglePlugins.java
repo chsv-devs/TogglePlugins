fb4ec4a3d13fff994faec44df1a7f6748a47056c
Hancho1577 <vojougae35@gmail.com>
2020-03-26 12:49:35 +0700

    update

--- src/main/java/hancho/TogglePlugins/TogglePlugins.java
+++ src/main/java/hancho/TogglePlugins/TogglePlugins.java
@@ -6,48 +6,45 @@ import cn.nukkit.event.Listener;
 import cn.nukkit.plugin.Plugin;
 import cn.nukkit.plugin.PluginBase;
 
-public class TogglePlugins extends PluginBase implements Listener{
+public class TogglePlugins extends PluginBase implements Listener {
 	@Override
 	public void onEnable() {
 		this.getServer().getPluginManager().registerEvents(this, this);
 	}
-	
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equals("loadplugin")) {
-			if(args.length < 1) {
+			if (args.length < 1) {
 				sender.sendMessage("§f§l[ §c! §f] /loadplugin <PluginFileName>");
 				return false;
 			}
-			if(this.getServer().getPluginManager().loadPlugin(this.getServer().getPluginPath() + "/" + args[0] + ".jar") == null) {
+
+			if (this.getServer().getPluginManager()
+					.loadPlugin(this.getServer().getPluginPath() + "/" + args[0] + ".jar") == null) {
 				sender.sendMessage("§f§l[ §c! §f] Could not load " + args[0] + ".jar");
-			}else {
+			} else {
 				sender.sendMessage("§f§l[ §6! §f] Successfully loaded.");
 			}
+
 			return true;
-		}else if (cmd.getName().equals("unloadplugin")) {
-			if(args.length < 1) {
+		} else {
+			if (args.length < 1) {
 				sender.sendMessage("§f§l[ §c! §f] /unloadplugin <PluginName>");
 				return false;
 			}
+
 			Plugin plugin = this.getServer().getPluginManager().getPlugin(args[0]);
-			if(plugin == null) {
+
+			if (plugin == null) {
 				sender.sendMessage("§f§l[ §c! §f] Could not find " + args[0]);
 				return false;
 			}
-			plugin.onDisable();
-			this.getServer().getPluginManager().disablePlugin(plugin);
-			sender.sendMessage("§f§l[ §6! §f] Successfully unloaded.");
-		}else if (cmd.getName().equals("forceunloadplugin")) {
-			if(args.length < 1) {
-				sender.sendMessage("§f§l[ §c! §f] /forceunloadplugin <PluginName>");
-				return false;
+
+			if (cmd.getName().equals("unloadplugin")) {
+				plugin.onDisable();
 			}
-			Plugin plugin = this.getServer().getPluginManager().getPlugin(args[0]);
-			if(plugin == null) {
-				sender.sendMessage("§f§l[ §c! §f] Could not find " + args[0]);
-				return false;
-			}
+
 			this.getServer().getPluginManager().disablePlugin(plugin);
 			sender.sendMessage("§f§l[ §6! §f] Successfully unloaded.");
 		}
